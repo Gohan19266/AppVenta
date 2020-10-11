32227e3b5e92c716113317bd2e564f676af8d281
Gohan19266 <johanmamani@upeu.edu.pe>
2020-10-11 13:01:03 -0500

    eventos

--- app/src/main/java/learn/mode/appventa/MainActivity.java
+++ app/src/main/java/learn/mode/appventa/MainActivity.java
@@ -2,7 +2,9 @@ package learn.mode.appventa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void play(View view){
+        Intent intent= new Intent(this,PrincipalActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
